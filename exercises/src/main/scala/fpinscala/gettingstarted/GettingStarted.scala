626d5d64f8ce625dafdd60efd2aa287dc00f2200
Christian Rasmussen <chrnr@hotmail.com>
2015-05-08 21:42:10 +0200

    did fibonacci exercise

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,13 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    def helper(count,first,second) {
+      if (count <= 1) first
+      else helper(count-1,second, first+second)
+    }
+    helper(n,0,1)
+  }//???
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
