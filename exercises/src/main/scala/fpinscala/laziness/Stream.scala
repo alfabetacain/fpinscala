74d6a2858e04e1e2bbcc2c0d6a30203744fe5994
Christian Rasmussen <chrnr@hotmail.com>
2015-06-02 08:35:33 +0200

    did some of the laziness exercises

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,6 +3,13 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
+  def toList: List[A] = 
+   this match {
+     case Cons(h,t) => h() :: t().toList
+     case Empty => List.empty
+   } 
+  
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
@@ -17,18 +24,45 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = 
+    if (n > 0) 
+      this match {
+        case Cons(h,t) => cons(h(),t().take(n-1))
+        case Empty => Empty
+      }
+    else Empty
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def drop(n: Int): Stream[A] = 
+    if (n > 0) 
+      this match {
+        case Cons(h,t) => t().drop(n-1)
+        case Empty => Empty
+      }
+    else this
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def takeWhile(p: A => Boolean): Stream[A] = 
+    foldRight(Stream.empty: Stream[A])((a,b) => if (p(a)) cons(a, b) else b)
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  def forAll(p: A => Boolean): Boolean = 
+    foldRight(true)((a,b) => p(a) && b)
 
-  def headOption: Option[A] = sys.error("todo")
+  def headOption: Option[A] = 
+    foldRight(None: Option[A])((a,b) => Some(a))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
+
+  def map[B](f: A => B): Stream[B] = 
+    foldRight(empty[B])((a,b) => cons(f(a), b))
+
+  def filter(p: A => Boolean): Stream[A] = 
+    foldRight(empty[A])((a,b) => if (p(a)) cons(a,b) else b)
+
+  def append[B >: A](s: => Stream[B]): Stream[B] = 
+    foldRight(s: Stream[B])((a,b) => cons(a, b))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = 
+    foldRight(empty[B])((a,b) => f(a) append(b))
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
 }
@@ -49,7 +83,12 @@ object Stream {
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = sys.error("todo")
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+  def from(n: Int): Stream[Int] = cons(n,from(n+1))
+  def fibs(a: Int,b: Int): Stream[Int] = 
+    def go(a: Int, b: Int): Stream[Int] = 
+      cons(a, go(b,a+b))
+    go(0,1)
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
 }
---
+++ exercises/src/main/scala/fpinscala/laziness/StreamTest.scala
@@ -0,0 +1,148 @@
+import org.scalatest._
+import org.scalatest.Assertions._
+import fpinscala.laziness._
+
+class StreamTest extends FlatSpec {
+
+  "toList" should "should turn a stream into a list, thus evaluating everything" in {
+    val s = Stream(1,2,3)
+    val actual = s.toList
+    assert(List(1,2,3) == actual)
+  }
+
+  "take" should "return the first n elements of a stream" in {
+    val s = Stream(1,2,3,4)
+    val actual = s.take(3).toList
+    assert(List(1,2,3) == actual)
+  }
+
+  it should "return empty if no elements are present" in {
+    val s = Stream.empty
+    val actual = s.take(2)
+    assert(Stream.empty == actual)
+  }
+  
+  it should "return as many elements as possible if n is higher than the stream length" in {
+    val s = Stream(1,2)
+    val actual = s.take(3).toList
+    assert(List(1,2) == actual)
+  }
+
+  "drop" should "drop the first n elements of a stream" in {
+    val s = Stream(1,2,3)
+    val actual = s.drop(2).toList
+    assert(List(3) == actual)
+  }
+
+  it should "return empty when there are no elements to drop" in {
+    val s = Stream.empty
+    val actual = s.drop(2)
+    assert(Stream.empty == actual)
+  }
+
+  it should "drop as many elements as possible if n is higher than the stream length" in {
+    val s = Stream(1,2)
+    val actual = s.drop(4)
+    assert(Stream.empty == actual)
+  }
+
+  "takeWhile" should "return the prefix of a stream that satisfies a given predicate" in {
+    val s = Stream(1,2,3)
+    val actual = s.takeWhile(_ < 3).toList
+    assert(List(1,2) == actual)
+  }
+
+  it should "return everything if everything matches predicate" in {
+    val s = Stream(1,2,3)
+    val actual = s.takeWhile(_ < 5).toList
+    assert(List(1,2,3) == actual)
+  }
+
+  it should "return empty if the first element does not satisfy predicate" in {
+    val s = Stream(1,2,3)
+    val actual = s.takeWhile(_ < 0)
+    assert(Stream.empty == actual)
+  }
+
+  it should "return empty if the stream is empty" in {
+    val s = Stream.empty: Stream[Int]
+    val actual = s.takeWhile(_ < 3)
+    assert(Stream.empty == actual)
+  }
+
+  "forAll" should "check if all elements satisfy predicate" in {
+    val s = Stream(1,2,3,4)
+    val actual = s.forAll(_ < 5)
+    assert(true == actual)
+  }
+
+  "headOption" should "return the first element of a nonempty stream" in {
+    val s = Stream(1,2)
+    val actual = s.headOption
+    assert(Some(1) == actual)
+  }
+
+  it should "return none if stream is empty" in {
+    val s = Stream.empty
+    val actual = s.headOption
+    assert(None == actual)
+  }
+
+  "map" should "map each element in a stream to something" in {
+    val s = Stream(1,2,3)
+    val actual = s.map(_ + 2).toList
+    assert(List(3,4,5) == actual)
+  }
+
+  it should "return empty when used on an empty stream" in {
+    val actual = (Stream.empty: Stream[Int]).map(_ + 2)
+    assert(Stream.empty == actual)
+  }
+
+  "filter" should "return the elements satisfying the given predicate" in {
+    val s = Stream(1,2,3)
+    val actual = s.filter(_ % 2 != 0).toList
+    assert(List(1,3) == actual)
+  }
+
+  it should "return empty when used on an empty stream" in {
+   val actual = (Stream.empty: Stream[Int]).filter(_ % 2 == 0)
+   assert(Stream.empty == actual)
+  }
+
+  "append" should "append the second argument to the first" in {
+    val s1 = Stream(1,2,3)
+    val s2 = Stream(4,5,6)
+    val actual = s1.append(s2).toList
+    assert(List(1,2,3,4,5,6) == actual)
+  }
+
+  it should "return empty when appending empty to empty" in {
+    val actual = Stream.empty.append(Stream.empty)
+    assert(Stream.empty == actual)
+  }
+
+  "flatMap" should "work" in {
+    val s = Stream(1,2)
+    val actual = s.flatMap(x => Stream(x,x)).toList
+    assert(List(1,1,2,2) == actual)
+  }
+
+  "constant" should "return a stream of constants" in {
+    val s = Stream.constant("Hello")
+    val actual = s.take(2).toList
+    assert(List("Hello","Hello") == actual)
+  }
+
+  "from" should "return a stream of increasing integers" in {
+    val s = Stream.from(1)
+    val actual = s.take(3).toList
+    assert(List(1,2,3) == actual)
+  }
+
+  "fibs" should "return the fibonacci sequence" in {
+    val s = Stream.fibs(0,1)
+    val actual = s.take(3).toList
+    assert(List(0,1,1) == actual)
+  }
+}
