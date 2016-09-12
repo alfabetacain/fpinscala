8447182452ac60cdc04978d3a43cddffd808cccc
Lauritz Hilsøe <mail@lauritz.me>
2016-09-12 14:19:09 +0200

    Semi-implemented JSON parser combinator

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -5,7 +5,64 @@ import scala.util.matching.Regex
 import fpinscala.testing._
 import fpinscala.testing.Prop._
 
-trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+trait JSON
+object JSON {
+  case object JNull extends JSON
+  case class JNumber(get: Double) extends JSON
+  case class JString(get: String) extends JSON
+  case class JBool(get: Boolean) extends JSON
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case class JObject(get: Map[String, JSON]) extends JSON
+
+  def jsonParser[Err, Parser[+_]] (P: Parsers[Err, Parser]): Parser[JSON] = {
+    import P._
+    val spaces: Parser[String] = operators(char(' ')).many.slice
+
+    val QUOTED: Parser[String] =
+      """"([^"]*)"""".r
+        .map { _ dropRight 1 substring 1 }
+
+    val DOUBLE: Parser[Double] =
+      """\d+(\.\d+)?""".r
+        .map { _.toDouble }
+
+    val WS: Parser[Unit] =
+      "[\t\n ]+".r map { _ => () }
+
+    val jnull: Parser[JSON] =
+      "null" |* succeed (JNull)
+
+    val jbool: Parser[JBool] =
+      (WS.? |* "true"  |* succeed(JBool(true))) |
+      (WS.? |* "false" |* succeed(JBool(false)))
+
+    val jstring: Parser[JString] =
+      QUOTED map { JString(_) }
+
+    val jnumber: Parser[JNumber] =
+      DOUBLE map { JNumber(_) }
+
+    lazy val jarray: Parser[JArray] =
+      (
+        WS.? |* "[" |* (json *| "," ).*
+        *| WS.? *| "]" *| WS.?
+      ) map { l => JArray(l.toVector) }
+
+    lazy val field: Parser[(String, JSON)] =
+      WS.? |* QUOTED *| WS.? *| ":" *| WS.? ** json *| ","
+
+    lazy val jobject: Parser[JObject] =
+      (WS.? |* "{" |* field.* *| WS.? *| "}" *| WS.?)
+        .map { l => JObject(l.toMap) }
+
+    lazy val json: Parser[JSON] =
+      ( jstring | jobject | jarray | jnull | jnumber | jbool) *| WS.?
+
+    return json
+  }
+}
+
+trait Parsers[ParseError, Parser[+_]] { self => // so inner classes may call methods of trait
 
   def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
   implicit def string(s: String): Parser[String]
@@ -51,11 +108,26 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   def char(c: Char): Parser[Char] = 
     map(string(c.toString))(_.charAt(0))
 
+  def option[A](p: Parser[A]): Parser[Option[A]]
+
+  def orL[A, B](p: Parser[A], p2: Parser[B]): Parser[A]
+  def orR[A, B](p: Parser[A], p2: Parser[B]): Parser[B]
+
+  def tuple[A, B](p: Parser[A], p2: Parser[B]): Parser[(A, B)] =
+    map2(p, p2)((a, b) => (a, b))
+
   case class ParserOps[A](p: Parser[A]) {
+    def ?(): Parser[Option[A]] = self.option(p)
     def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
+    def *|[B](p2: Parser[B]): Parser[A] = self.orL(p,p2)
+    def |*[B](p2: Parser[B]): Parser[B] = self.orR(p,p2)
     def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
     def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def many: Parser[List[A]] = self.many(p)
+    def *(): Parser[List[A]] = self.many(p)
+    def slice: Parser[String] = self.slice(p)
+    def **[B](p2: Parser[B]): Parser[(A, B)] = self.tuple(p,p2)
   }
 
   object Laws {
